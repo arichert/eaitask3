120cd56fabb7f469089ec7122cbfdb1c8a269675
Steffen Schildknecht <s_schi32@uni-muenster.de>
2010-06-14 01:38:41 +0200

    adds initial data...java has too much overhead...

--- src/java/de/wwu/database/SQLite.java
+++ src/java/de/wwu/database/SQLite.java
@@ -1,7 +1,12 @@
 package de.wwu.database;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.io.File;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.LogManager;
 
 /**
  * Create and connect to a SQLite database
@@ -13,6 +18,34 @@ public class SQLite {
 
     public Connection conn;
     public String pathToDbFile = System.getProperty("user.dir") + "/test.db";
+
+
+    public HashMap[] getExamples()
+    {
+        HashMap car_example  = new HashMap();
+        HashMap bike_example = new HashMap();
+        HashMap product_example = new HashMap();
+
+        HashMap[] examples = {car_example,bike_example,product_example};
+
+        car_example.put("domain", "cars");
+        car_example.put("brand", "VW");
+        car_example.put("price", "10 Euro");
+        car_example.put("id", null);
+
+        bike_example.put("domain", "bikes");
+        bike_example.put("brand", "Holland");
+        bike_example.put("price", "5 Euro");
+        bike_example.put("id", null);
+
+        product_example.put("domain", "products");
+        product_example.put("brand", "Becks");
+        product_example.put("price", "2 Euro");
+        product_example.put("id", null);
+
+        return examples;
+    }
+
 
     /**
      * Connect to a database
@@ -55,6 +88,9 @@ public class SQLite {
      */
     public void initialSetup() {
         try {
+
+            HashMap[] examples = getExamples();
+            
             Statement stat = this.conn.createStatement();
 
             //enable foreign key restraints (works if compiled that way, precompiled binaries should be allright)
@@ -67,18 +103,7 @@ public class SQLite {
                     "CREATE TABLE domain (" +
                     "name VARCHAR(200) PRIMARY KEY NOT NULL);");
 
-            PreparedStatement prep = this.conn.prepareStatement(
-                    "insert into domain values (?);");
-
-            prep.setString(1, "cars");
-            prep.addBatch();
-            prep.setString(1, "products");
-            prep.addBatch();
-            prep.setString(1, "bikes");
-            prep.addBatch();
-
             this.conn.setAutoCommit(false);
-            prep.executeBatch();
             this.conn.setAutoCommit(true);
 
 
@@ -91,17 +116,6 @@ public class SQLite {
                                "FOREIGN KEY (domain_name)   REFERENCES domain (name)," +
                                "PRIMARY KEY (attribute_name, domain_name) );");
 
-            prep = this.conn.prepareStatement("insert into attribute (attribute_name, domain_name) values (?, ?);");
-
-            prep.setString(1, "brand");
-            prep.setString(2, "cars");
-            prep.addBatch();
-
-
-            conn.setAutoCommit(false);
-            prep.executeBatch();
-            conn.setAutoCommit(true);
-
             //Item
             stat = this.conn.createStatement();
             stat.executeUpdate("DROP TABLE if exists item;");
@@ -112,11 +126,6 @@ public class SQLite {
                     "domain_name VARCHAR(200) NOT NULL," +
                     "FOREIGN KEY (domain_name)   REFERENCES domain (name) );");
 
-
-            conn.setAutoCommit(false);
-            prep.executeBatch();
-            conn.setAutoCommit(true);
-
             //Value
             stat = this.conn.createStatement();
             stat.executeUpdate("DROP TABLE if exists value;");
@@ -126,15 +135,87 @@ public class SQLite {
                     "item_id INTEGER, " +
                     "domain_name VARCHAR(200)," +
                     "attribute_name VARCHAR(200)," +
+                    "value TEXT," +
                     "FOREIGN KEY (domain_name)   REFERENCES domain (name),"+
                     "FOREIGN KEY (attribute_name)   REFERENCES attribute(attribute_name)," +
                     "FOREIGN KEY (item_id)   REFERENCES item(item_id)," +
                     "PRIMARY KEY (item_id, domain_name, attribute_name));");
 
 
+
+
+            //Add domains
+            PreparedStatement prep = this.conn.prepareStatement("insert into domain values (?);");
+            for (HashMap item : examples) {
+                prep.setString(1, item.get("domain").toString());
+                prep.addBatch();
+            }
             conn.setAutoCommit(false);
             prep.executeBatch();
             conn.setAutoCommit(true);
+
+            //Add Attributes
+            prep = this.conn.prepareStatement("insert into attribute (attribute_name, domain_name) values (?, ?);");
+
+            Iterator i;
+            for (HashMap item : examples) {
+                i =  item.keySet().iterator();
+                String domain = item.get("domain").toString();
+                while( i. hasNext() ){
+                    String key = i.next().toString();
+                    if ((!key.equals("domain")) && (!key.equals("id")))
+                    {
+                        prep.setString(1, key);
+                        prep.setString(2, domain);
+                        prep.addBatch();
+                    }
+                }
+            }
+            conn.setAutoCommit(false);
+            prep.executeBatch();
+            conn.setAutoCommit(true);
+
+            //Insert Items - retrieve inserted IDs ... NOTE: Is not safe with multiple clients (use transactions?)
+            prep = this.conn.prepareStatement("insert into item (domain_name) values (?);");
+            for (HashMap item : examples) {
+                prep.setString(1, item.get("domain").toString());
+                prep.execute();
+                ResultSet lastId = stat.executeQuery("SELECT last_insert_rowid();");
+                lastId.next();
+
+                //TODO : Remove
+                String bla = lastId.getString(1);
+                System.out.println("This is bla: " + bla);
+                item.put("id", lastId.getString(1));
+                lastId.close();
+            }
+
+            //Insert Values
+            prep = this.conn.prepareStatement("insert into value (item_id, attribute_name, domain_name,value) values (?,?,?,?);");
+            for (HashMap item : examples) {
+                i =  item.keySet().iterator();
+                String id = item.get("id").toString();
+                String domain = item.get("domain").toString();
+                System.out.println(id + "     " + domain);
+                
+                while( i.hasNext() ){
+                    String key = i.next().toString();
+                    if ((!key.equals("domain")) && (!key.equals("id"))) {
+                    prep.setString(1, id);
+                    prep.setString(2, key);
+                    prep.setString(3, domain);
+                    prep.setString(4, item.get(key).toString());
+
+                    System.out.println(id + " " + key + " " + domain + " " + item.get(key).toString());
+                    prep.addBatch();
+                    }
+                }
+            }
+            conn.setAutoCommit(false);
+            prep.executeBatch();
+            conn.setAutoCommit(true);
+
+
 
         } catch (Exception e) {
             System.out.println(e);
